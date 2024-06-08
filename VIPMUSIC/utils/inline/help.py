15c226f2252b8730f75e24d0b8fdd455c112212d
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-06-08 09:52:04 +0530

    Update help.py

--- VIPMUSIC/utils/inline/help.py
+++ VIPMUSIC/utils/inline/help.py
@@ -5,8 +5,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 def first_page(_):
     controll_button = [
-        InlineKeyboardButton(text="๏ ᴍᴇɴᴜ ๏", callback_data=f"settingsback_helper"),
-        InlineKeyboardButton(text="๏ ɴᴇxᴛ ๏", callback_data=f"dilXaditi"),
+        InlineKeyboardButton(text="ᴍᴇɴᴜ", callback_data=f"settingsback_helper"),
+        InlineKeyboardButton(text="ɴᴇxᴛ", callback_data=f"dilXaditi"),
     ]
     first_page_menu = InlineKeyboardMarkup(
         [
@@ -41,7 +41,7 @@ def first_page(_):
 
 def second_page(_):
     controll_button = [
-        InlineKeyboardButton(text="๏ ʙᴀᴄᴋ ๏", callback_data=f"settings_back_helper")
+        InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settings_back_helper")
     ]
     second_page_menu = InlineKeyboardMarkup(
         [
@@ -102,55 +102,55 @@ def help_pannel(_, START: Union[bool, int] = None):
         [
             [
                 InlineKeyboardButton(
-                    text="🍁αԃɱιɳ🍁",
+                    text="ᴀᴅᴍɪɴ",
                     callback_data="helpcallback hb1",
                 ),
                 InlineKeyboardButton(
-                    text="🔺αυƭɦ🔺",
+                    text="ᴀᴜᴛʜ",
                     callback_data="helpcallback hb2",
                 ),
                 InlineKeyboardButton(
-                    text="♨️вℓσ¢к♨️",
+                    text="ʙʟᴏᴄᴋ",
                     callback_data="helpcallback hb3",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="📣ɠ¢αʂƭ📣",
+                    text="ɢᴄᴀsᴛ",
                     callback_data="helpcallback hb4",
                 ),
                 InlineKeyboardButton(
-                    text="🚫ɠɓαɳ🚫",
+                    text="ɢʙᴀɴ",
                     callback_data="helpcallback hb12",
                 ),
                 InlineKeyboardButton(
-                    text="🍷ℓყɾเ¢ʂ🍷",
+                    text="ʟʏʀɪᴄs",
                     callback_data="helpcallback hb5",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="🎙️քℓαყℓเʂƭ🎙️",
+                    text="ᴘʟᴀʏʟɪsᴛ",
                     callback_data="helpcallback hb6",
                 ),
                 InlineKeyboardButton(
-                    text="🎸ѵσเ¢ε-¢ɦαƭ🎸",
+                    text="ᴠᴏɪᴄᴇ-ᴄʜᴀᴛ",
                     callback_data="helpcallback hb10",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="🕹️ρℓαყ🕹️",
+                    text="ᴘʟᴀʏ",
                     callback_data="helpcallback hb8",
                 ),
                 InlineKeyboardButton(
-                    text="🍸ʂ𝖚∂σ🍸",
+                    text="sᴜᴅᴏ",
                     callback_data="helpcallback hb9",
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text="⚜️SƬΛᏒƬ⚜️",
+                    text="sᴛᴀʀᴛ",
                     callback_data="helpcallback hb11",
                 ),
             ],
@@ -179,7 +179,7 @@ def private_help_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🎭 𝐇𝐄𝐋𝐏 🎭",
+                text="Hᴇʟᴘ",
                 callback_data="settings_back_helper",
             ),
         ],
